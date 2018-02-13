97bf3d839ccc8c8fe8be83aac9fbd6a04d231e4a
gsvr <george@westerncapelabs.com>
2018-02-13 10:33:32 +0200

    add DEBIT_CONFIG testsetting

--- backend/maguire/testsettings.py
+++ backend/maguire/testsettings.py
@@ -26,3 +26,6 @@ POSTMARK_SERVER_TOKEN = 'POSTMARK_API_TEST'
 EMAIL_DEVOPS = os.environ.get('EMAIL_DEVOPS', 'devops@example.com')
 
 BASE_URL = os.environ.get('BASE_URL', 'http://localhost:8000')
+
+DEBIT_CONFIG = json.loads(os.environ.get('DEBIT_CONFIG', '{"base_url": "https://www.slowdebit.co.za:8888/Services/PaymentService.svc/PartnerServices/","authentication": {"service_reference": "37H2-F00Z-735Q-1B11","username": "uname"},"bank_ref": "PICSA","group_code": "PICSA"}'))
+
