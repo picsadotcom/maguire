b8f86e162124a873b94419e7bdc8a35ff8e52321
Mike Jones <imsickofmaps@users.noreply.github.com>
2018-02-16 14:29:59 +0000

    Better django admin filters on debits

--- backend/debits/admin.py
+++ backend/debits/admin.py
@@ -6,13 +6,13 @@ from .models import Debit
 @admin.register(Debit)
 class DebitAdmin(VersionAdmin):
     list_display = [
-        "id", "client", "downstream_reference", "reference", "provider_reference",
+        "id", "client", "status", "downstream_reference", "reference", "load_attempts",
         "scheduled_at", "loaded_at", "created_at", "updated_at",
-        "account_name", "account_number", "branch_code", "account_type",
+        "provider_reference", "account_name", "account_number", "branch_code", "account_type",
         "amount", "callback_url", "node_id",
     ]
     list_filter = [
-        "branch_code", "account_type", "provider",
+        "status", "load_attempts", "branch_code", "account_type", "provider",
         "scheduled_at", "loaded_at", "created_at", "updated_at",
     ]
     search_fields = [
