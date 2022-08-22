169746eea4f8b520af9b7388ae62cdf903dd288b
gsvr <gsvanrooyen@gmail.com>
2022-08-22 20:18:47 +0200

    remove redundant lambda usage

--- backend/maguire/celery.py
+++ backend/maguire/celery.py
@@ -1,10 +1,7 @@
 from __future__ import absolute_import
 
 import os
-
 from celery import Celery
-
-from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maguire.settings')
@@ -14,7 +11,7 @@ app = Celery('maguire')
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
 
 @app.task(bind=True)
